a644528c6120e298626e4da2b6836e76e19e1bea
rumslaw <rumslaw@gmail.com>
2020-12-04 22:56:22 +0100

    blog-posts-page Implemented blog posts page with filtering by topic

--- src/main/java/pl/sda/blogservicedata/controller/BlogPostPageController.java
+++ src/main/java/pl/sda/blogservicedata/controller/BlogPostPageController.java
@@ -1,13 +1,36 @@
 package pl.sda.blogservicedata.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.sda.blogservicedata.model.BlogPost;
+import pl.sda.blogservicedata.model.Topic;
+import pl.sda.blogservicedata.service.BlogPostService;
+import pl.sda.blogservicedata.service.TopicService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class BlogPostPageController {
 
+    private BlogPostService blogPostService;
+    private TopicService topicService;
+
+    public BlogPostPageController(BlogPostService blogPostService, TopicService topicService) {
+        this.blogPostService = blogPostService;
+        this.topicService = topicService;
+    }
+
     @GetMapping("/blogPosts")
-    public String blogPostsPage() {
+    public String blogPostsPage(ModelMap modelMap, @RequestParam(required = false) String topic) {
+        List<BlogPost> blogPosts = topic == null ? blogPostService.findAll() :
+                blogPostService.findByFilter(topic, null, null, null);
+ //       List<BlogPost> blogPosts = blogPostService.findAll();
+        List<String> topics = topicService.findAll().stream().map(Topic::getName).collect(Collectors.toList());
+        modelMap.addAttribute("blogPosts", blogPosts);
+        modelMap.addAttribute("topics", topics);
         return "blogPosts";
     }
 }
